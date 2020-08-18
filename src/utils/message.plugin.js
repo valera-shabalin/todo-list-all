81a96f99fa5503a876b7487e2015cf4ab730a724
Valera Shabalin <shabalinvalera.ts@gmail.com>
2020-08-18 19:10:28 +0700

    Message plugin

--- src/utils/message.plugin.js
+++ src/utils/message.plugin.js
@@ -1,7 +1,27 @@
 export default {
 	install(Vue, options) {
-		Vue.prototype.$message = function(html) {
-			console.log(html)
+		Vue.prototype.$message = function(title) {
+			let modal = document.createElement('div')
+			modal.innerHTML = ` 
+				<div class="modal-window">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-lg-8">
+								<div class="modal-window__form">
+									<a href="#" class="close"">Закрыть</a>
+									<h2>${title}</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			`
+			document.body.appendChild(modal)
+			let close = document.body.querySelector('.modal-window .close')
+			close.addEventListener('click', (e) => {
+				e.preventDefault()
+				document.body.removeChild(modal)
+			}) 
 		}
 
 		Vue.prototype.$error = function(html) {
